c44e2f2e60fa35c908102988f73ff5b64c343d92
Gustavo de Oliveira <gustavofarrias90@gmail.com>
2025-08-05 15:38:58 -0300

    Criação das classes pet e Menu Incial e os pacotes service e model, e ArquivoReader ajustado para service

---
+++ src/model/Pet.java
@@ -0,0 +1,57 @@
+package model;
+
+public class Pet {
+    private String nome;
+    private String raca;
+    private int idade;
+    private double peso;
+
+    public Pet(String nome, String raca, int idade, double peso) {
+        this.nome = nome;
+        this.raca = raca;
+        this.idade = idade;
+        this.peso = peso;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getRaca() {
+        return raca;
+    }
+
+    public void setRaca(String raca) {
+        this.raca = raca;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    @Override
+    public String toString() {
+        return "Pet{" +
+                "nome='" + nome + '\'' +
+                ", raca='" + raca + '\'' +
+                ", idade=" + idade +
+                ", peso=" + peso +
+                '}';
+    }
+}
